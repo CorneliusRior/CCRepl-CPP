55b5f199c33e6a1a2b677210fedb379453d72dab
CorneliusRior <corneliuscriordan@gmail.com>
2026-06-09 21:25:11 +0100

    Add CCRepl.h & util.h to git tracking

---
+++ include/CCRepl/CCRepl.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <CCRepl/BaseCommands.h>
+#include <CCRepl/CommandArgs.h>     // not sure if this is needed
+#include <CCRepl/CommandBuilder.h>
+#include <CCRepl/CommandSet.h>
+#include <CCRepl/IArgSpec.h>
+#include <CCRepl/ReplCommand.h>
+#include <CCRepl/ReplContext.h>
+#include <CCRepl/ReplException.h>
+#include <CCRepl/ScptCommands.h>
+#include <CCRepl/Script.h>
+#include <CCRepl/ScriptService.h>
+#include <CCRepl/Tokenizers.h>
---
+++ include/util/util.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <util/str.h>
+#include <util/fmt.h>
+#include <util/parsers.h>
