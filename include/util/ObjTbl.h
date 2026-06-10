4a53d07e8d8edf74ac42514ccaaf9165b39c98d7
CorneliusRior <corneliuscriordan@gmail.com>
2026-06-10 04:42:33 +0100

    Put in place ObjTbl

---
+++ include/util/ObjTbl.h
@@ -0,0 +1,29 @@
+#include <util/str.h>
+#include <util/fmt.h>
+
+namespace fmt {    
+/*
+    class IObjColumn {
+    public:
+        virtual ~IObjColum() = default;
+        std::string Header;
+        std::size_t Width;
+        TextAlign HeaderAlignment = TextAlign::Left;
+        TextAlign DataAlignment = TextAlign::Left;
+        virtual std::string Render() const = 0;
+        
+    }
+
+
+    
+    //Something of an attempt to make a more advanced version of TextTable: Instead of it just being strings, it actually contrains a list of objects, which enables you to do things like select by row (int) and automatically order by one row or another.
+    
+    template <typename Obj>
+    class ObjTable {
+    private:
+
+    public:
+
+    };
+*/
+}
